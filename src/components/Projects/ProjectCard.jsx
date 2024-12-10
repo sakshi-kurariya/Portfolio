e06083e60dfdfbcd50127dd2caee32f876762215
sakshi-kurariya <sakshikurariya2017@gmail.com>
2024-12-10 17:20:39 +0530

    icon changed

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -13,7 +13,7 @@ export const ProjectCard = ({
         alt={`Image of ${title}`}
         className={styles.image}
       />
-      <h3 className={styles.title}>{title}</h3>
+      <h3  className={styles.title}>{title}</h3>
       <p className={styles.description}>{description}</p>
       <ul className={styles.skills}>
         {skills.map((skill, id) => {
